df6c4beba43544e848d54429b87b037187302022
Samuel Moreno Vahos <95778014+Samuel0104@users.noreply.github.com>
2022-09-12 20:23:21 -0500

    Update solver.hpp

--- headers/solver.hpp
+++ headers/solver.hpp
@@ -6,19 +6,19 @@
 
 class Solver{
 private:
-	Cube* rubik; // Apuntador a un objeto Cube
+	Cube* rubik; // Pointer to a Cube object
 	std::vector<char> solution;
-	char faceRestrict; // Se restringen las posibles ramas del backtracking despues de armar una pareja
+	char faceRestrict; // Possible backtracking branches are restricted after solving a pair
 
 //---------------------Metodos encargados de hacer los movimientos del cubo---------------------//
-	// Recibe un caracter y rota la cara del cubo asociada a ese caracter
+	// Takes a character and rotates the cube's face associated with that character
 	void makeMove(char m);
 	
-	// Deshace la ultima rotacion hecha en el cubo
+	// Undoes the last rotation made on the cube
 	void retractMove();
 //----------------------------------------------------------------------------------------------//
 
-	// Simplifica la implementacion de la funcion getOll()
+	// Simplifies implementation of the getOll() function
 	bool ollHelper(int a, int b, int c, int d);
 	
 	
@@ -26,42 +26,42 @@ private:
 
 public:
 	/* Constructor:
-		Recibe un objeto de clase Cube para
-		apuntar a su direccion en memoria.
+		Takes a Cube object to point
+		to its memory address
 	*/
 	Solver(Cube& c);
 	
 //-----------------Metodos encargados de la solucion de la totalidad del cubo-------------------//
-	// Se arma todo el cubo con backtracking
+	// The whole cube is solved with backtracking
 	bool solve(unsigned depth);
 	
-	// Encapsula la anterior
+	// Encapsulates the above
 	void wholeCube();
 //----------------------------------------------------------------------------------------------//
 	
 //---------------Metodos encargados de la solucion de la capa inferior del cubo-----------------//
-	// Se juntan dos piezas en su posicion correcta en la parte inferior del cubo
+	// Two pieces are put together in their correct position at the bottom of the cube
 	bool pair(unsigned depth);
 	
-	// Se termina de armar la capa inferior del cubo
+	// The rest of the bottom layer is solved
 	bool face(unsigned depth);
 	
-	// Encapsula las anteriores
+	// Encapsulates the above
 	void firstLayer();
 //----------------------------------------------------------------------------------------------//
 
 //---------------Metodos encargados de la solucion de la capa superior del cubo-----------------//
-	// Identifica el caso de oll necesitado en la actual resolucion del cubo
+	// Identifies the OLL case needed in the current state of the cube
 	std::string getOll();
 	
-	// Identifica el caso de pll necesitado en la actual resolucion del cubo
+	// Identifies the PLL case needed in the current state of the cube
     std::string getPll();
 	
-	// Encapsula las anterioes
+	// Encapsulates the above
 	void lastLayer();
 //----------------------------------------------------------------------------------------------//
 	
-	// Se imprimen la lista y el numero de rotaciones realizadas en el cubo
+	// The list and number of moves are displayed
 	void printSolution();
 };
 
